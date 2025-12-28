5e19d98cb19bdfa96507586925daa0041315140e
manujamnju-art <manujamnju@gmail.com>
2025-12-28 20:38:38 +0530

    Update test_attendance.py

--- test_attendance.py
+++ test_attendance.py
@@ -1,16 +1,8 @@
-import pytest
-import sys
 from attendance import calculate_attendance_percentage, get_exam_eligibility
 
-def test_percentage():
-    assert calculate_attendance_percentage(100, 80) == 80.0
+def test_attendance_percentage():
+    assert calculate_attendance_percentage(75, 100) == 75.0
 
-def test_eligible():
-    assert get_exam_eligibility(80) == "Eligible for Exam"
-
-def test_not_eligible():
-    assert get_exam_eligibility(60) == "Not Eligible for Exam"
-
-def test_invalid_total():
-    with pytest.raises(ValueError):
-        calculate_attendance_percentage(0, 10)
+def test_exam_eligibility():
+    assert get_exam_eligibility(80) == "Eligible"
+    assert get_exam_eligibility(60) == "Not Eligible"

a9ccb8706667d0da5ff529052e66645f1e920c71
manujamnju-art <manujamnju@gmail.com>
2025-12-28 20:46:07 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -15,11 +15,15 @@ if __name__ == "__main__":
         total_classes = int(sys.argv[2])
         attended_classes = int(sys.argv[3])
 
+       
+    else:
+        name = Manju
+        total_classes = 100
+        attended_classes = 90
+        
         percentage = calculate_attendance_percentage(attended_classes, total_classes)
         eligibility = get_exam_eligibility(percentage)
 
         print(f"Name: {name}")
         print(f"Attendance Percentage: {percentage:.2f}%")
         print(f"Status: {eligibility}")
-    else:
-        print("Usage: python attendance.py <name> <total_classes> <attended_classes>")
